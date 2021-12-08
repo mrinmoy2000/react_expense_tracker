35021b42a56ebed24af3c66669128182ad095258
Mrinmoy <mrin2ksarkar@gmail.com>
2021-12-08 17:38:40 +0530

    Initial Commit

--- src/components/Components/Expenses.js
+++ src/components/Components/Expenses.js
@@ -1,10 +1,21 @@
+import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpenseFilter from '../ExpenseFilter/ExpensesFilter'
+
+
 
 function Expenses(props) {
+
+  const [enteredFilter, setEnteredFilter] = useState('2020')
+  const filterChangeHandler = (selectedFilter) => {
+    setEnteredFilter(selectedFilter);
+  }
+
   return (
     <Card className="expenses">
+      <ExpenseFilter selected ={enteredFilter} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
