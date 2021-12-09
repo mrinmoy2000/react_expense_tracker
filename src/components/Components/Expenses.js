5599f0998129860ec5dd38a54aecb77669b7a7cd
Mrinmoy <mrin2ksarkar@gmail.com>
2021-12-09 13:09:08 +0530

    Initial Commit

--- src/components/Components/Expenses.js
+++ src/components/Components/Expenses.js
@@ -10,19 +10,29 @@ function Expenses(props) {
     setEnteredFilter(selectedFilter);
   };
 
+  const filteredExpenses = props.expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === enteredFilter
+  })
+
+  let expensesContent  = <p>No expenses found.</p>;
+  if(filteredExpenses.length > 0){
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={enteredFilter}
         onChangeFilter={filterChangeHandler}
       />
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 }
