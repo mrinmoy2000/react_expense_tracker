5ea9ac9af5bff5a812940e7d778b02a7db36efa7
Mrinmoy <mrin2ksarkar@gmail.com>
2021-12-09 11:47:51 +0530

    Initial Commit

--- src/components/Components/Expenses.js
+++ src/components/Components/Expenses.js
@@ -1,36 +1,28 @@
-import React, {useState} from 'react';
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
-import ExpenseFilter from '../ExpenseFilter/ExpensesFilter'
-
-
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpenseFilter from "../ExpenseFilter/ExpensesFilter";
 
 function Expenses(props) {
-
-  const [enteredFilter, setEnteredFilter] = useState('2020')
+  const [enteredFilter, setEnteredFilter] = useState("2020");
   const filterChangeHandler = (selectedFilter) => {
     setEnteredFilter(selectedFilter);
-  }
+  };
 
   return (
     <Card className="expenses">
-      <ExpenseFilter selected ={enteredFilter} onChangeFilter={filterChangeHandler} />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
+      <ExpenseFilter
+        selected={enteredFilter}
+        onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,7 +22,7 @@ const ExpenseForm = (props) => {
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: enteredDate,
+      date: new Date(enteredDate),
     };
 
     props.onSaveExpenseData(expenseData);
